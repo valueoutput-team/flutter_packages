b2b78677123ffcf1b237a592c9979fdfe9c61c45
Hitesh Verma <hverma822@gmail.com>
2024-10-17 12:13:12 +0530

    imported shimmer widget

--- lib/free_map.dart
+++ lib/free_map.dart
@@ -4,3 +4,4 @@ export 'widgets/fm_widget.dart';
 export 'services/fm_models.dart';
 export 'services/fm_service.dart';
 export 'widgets/fm_search_field.dart';
+export 'widgets/fm_shimmer_widget.dart';
