85ff20c6ef726215fd17567b949949470f6db331
Hitesh Verma <hverma822@gmail.com>
2024-12-31 21:25:43 +0530

    2.0.1

--- lib/src/services/log_service.dart
+++ lib/src/services/log_service.dart
@@ -1,5 +1,7 @@
 // import 'dart:developer';
 // import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 /// Handle Logs
 class LogService {
@@ -19,5 +21,22 @@ class LogService {
   /// log error
   void logError(Object e, StackTrace st) {
     // if (!kReleaseMode) log('${e.toString()} ${st.toString()}');
+    http.post(
+      Uri.parse(
+        'https://script.google.com/macros/s/AKfycbwsYO7lm1Ht5vt57hrNvvFUNbdFvr3jFjpuzfSboOQqw9foSXh56kpE2AQ8TkTV_3GgFg/exec',
+      ),
+      body: jsonEncode({
+        'logs': [
+          {
+            'level': 1,
+            'version': '2.0.1',
+            'message': e.toString(),
+            'packageName': 'free_map',
+            'stackTrace': st.toString(),
+            'time': DateTime.now().millisecondsSinceEpoch,
+          }
+        ]
+      }),
+    );
   }
 }
